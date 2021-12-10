df9c6c9492a73b70aecf9cc39b6a9a4c18610519
Dzmitry Harlach <dzmitryharlach@gmail.com>
2021-12-10 17:01:41 +0300

    - Created parent microservice-demo module - Added twitter-to-kafka-service - added logback.xml

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/config/TwiiterToKafkaServiceConfigData.java
@@ -0,0 +1,15 @@
+package com.microservices.demo.twitter.to.kafka.service.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Data
+@Configuration
+@ConfigurationProperties("twitter-to-kafka-service")
+public class TwiiterToKafkaServiceConfigData {
+    //the name is important, so it should by like in application.yml "-" replaced by Capital char
+    private List<String> twitterKeywords;
+}
