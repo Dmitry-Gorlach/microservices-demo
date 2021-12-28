ba5cb9690d6c12bd8afd0d9141cd4b4226e524cc
dmitry <dmitry_1@tut.by>
2021-12-28 16:08:53 +0300

    add jasypt test class

--- playground/src/main/java/jasypt/TestJasypt.java
+++ playground/src/main/java/jasypt/TestJasypt.java
@@ -8,9 +8,10 @@ public class TestJasypt {
     public static void main(String[] args) {
         StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
         standardPBEStringEncryptor.setPassword("Demo_Pwd!2020");
-        standardPBEStringEncryptor.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+        standardPBEStringEncryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
         standardPBEStringEncryptor.setIvGenerator(new RandomIvGenerator());
-        final String result = standardPBEStringEncryptor.encrypt("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");
+        standardPBEStringEncryptor.setStringOutputType("HEX");
+        String result = standardPBEStringEncryptor.encrypt("springCloud_Pwd!");
         System.out.println(result);
         System.out.println(standardPBEStringEncryptor.decrypt(result));
     }
